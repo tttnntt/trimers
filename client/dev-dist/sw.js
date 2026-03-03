4bf85f916a9f7cdfad29a733cce4ac2e4fc71f35
tttnntt <bejayjunior.1234@gmail.comgit config --global user.email>
2026-03-03 19:15:37 +0000

    add Vercel rewrites for API proxying

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6169d802'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tsurafh45es"
+    "revision": "0.o9k3chs59o4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
